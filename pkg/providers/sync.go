59a19ad70d43bdff59ea094cdb2fc8b999eed400
Tsvetoslav Dimov <baskski@gmail.com>
2022-11-16 20:02:21 +0000

    fix: syncLists exiting before watchlist is synced

--- pkg/providers/sync.go
+++ pkg/providers/sync.go
@@ -65,18 +65,19 @@ func (u *user) populateData(ic *imdb.Client, tc *trakt.Client) {
 func (u *user) syncLists(tc *trakt.Client) {
 	for _, list := range u.lists {
 		diff := list.Difference()
-		if len(diff["add"]) > 0 {
-			if list.IsWatchlist {
+		if list.IsWatchlist {
+			if len(diff["add"]) > 0 {
 				tc.WatchlistItemsAdd(diff["add"])
-				continue
 			}
+			if len(diff["remove"]) > 0 {
+				tc.WatchlistItemsRemove(diff["remove"])
+			}
+			continue
+		}
+		if len(diff["add"]) > 0 {
 			tc.ListItemsAdd(list.TraktListId, diff["add"])
 		}
 		if len(diff["remove"]) > 0 {
-			if list.IsWatchlist {
-				tc.WatchlistItemsRemove(diff["remove"])
-				continue
-			}
 			tc.ListItemsRemove(list.TraktListId, diff["remove"])
 		}
 	}
